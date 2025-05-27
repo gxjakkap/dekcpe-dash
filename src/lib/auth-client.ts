8f824539e55fbf570aac3674ac9c073573f7351c
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-05-27 09:06:37 +0700

    fix: im going back to nextauth i swear

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,19 @@
 import { createAuthClient } from "better-auth/react"
 
+const getBaseURL = () => {
+  const url = process.env.AUTH_URL || "http://localhost:4000";
+  
+  try {
+    new URL(url);
+    return url;
+  } 
+  catch {
+    return "http://localhost:4000"
+  }
+}
+
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:4000",
+    baseURL: getBaseURL(),
 })
 
 export const { signIn, signOut, signUp, useSession } = authClient
