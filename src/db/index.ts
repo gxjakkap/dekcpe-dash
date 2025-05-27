90f256c03ae3469c4a051aaa905e5d0a414a5915
Jakkaphat Chalermphanaphan <gunt@guntxjakka.me>
2025-05-27 09:31:47 +0700

    build: :)))))

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,7 @@
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
 
-const connectionString = `postgres://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DBNAME}`
+const connectionString = `postgres://${process.env.PG_USER ?? "postgres"}:${process.env.PG_PASSWORD ?? ""}@${process.env.PG_HOST ?? "localhost"}:${process.env.PG_PORT ?? "5432"}/${process.env.PG_DBNAME ?? "postgres"}`
 const pool = postgres(connectionString, { max: 1 })
 
 export const db = drizzle(pool)
